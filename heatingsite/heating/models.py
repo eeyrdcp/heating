14b8ad113ae31ca2f14b8ad09a05beef603ac695
David Parker <eeyrdcp@talk21.com>
2016-09-29 21:36:05 +0000

    create models

--- heatingsite/heating/models.py
+++ heatingsite/heating/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Zone(models.Model):
+    zone_name = models.CharField(max_length=200)
+
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=200)
+
+
+class TemperatureRecord(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    record_time = models.DateTimeField('Time of reading')
+    temperature = models.FloatField()
+    
+
+class Program(models.Model):
+    weekday = models.IntegerField()
+    on_time = models.TimeField()
+    off_time = models.TimeField()
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    set_temp = models.FloatField()
+
+class ZoneRecord(models.Model):
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    record_time = models.DateTimeField()
+    heating_on = models.BooleanField()
+
+
+
